6924f14e2e124fdee353a59d558b864d6addf08d
Paula Gutierrez Dias <gutierrez.012900@gmail.com>
2023-11-13 21:59:39 -0500

    ajustes del componente login, alerta e inicio con clinica service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,17 +8,22 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './pagina/inicio/inicio.component';
 import { LoginComponent } from './pagina/login/login.component';
 import { RegistroComponent } from './pagina/registro/registro.component';
+import { AlertaComponent } from './pagina/alerta/alerta.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    AlertaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pagina/login/login.component.ts
+++ src/app/pagina/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Alerta } from 'src/app/modelo/alerta';
+import { LoginDTO } from 'src/app/modelo/clinica/LoginDTO';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { ClinicaService } from 'src/app/servicios/clinica.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +12,47 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginDTO: LoginDTO;
+  alerta!: Alerta;
+  mostrarMensaje:string ="";
+  mostrarMess:boolean=false;
+
+  constructor(private tokenService: TokenService, private authService: AuthService, private clinicaService: ClinicaService) {
+    this.loginDTO = new LoginDTO();
+  }
+
+  public login() {
+    this.authService.login(this.loginDTO).subscribe({
+      next: data => {
+        this.tokenService.login(data.respuesta.token);
+
+      },
+      error: error => {
+        this.alerta = { mensaje: error.error.respuesta, tipo: "danger" };
+
+      }
+    });
+  }
+
+  enviarLinkRecuperacion(event: Event) {
+    event.preventDefault();
+
+    if(this.loginDTO.email==""){
+      this.mostrarMess=true;
+      this.mostrarMensaje="Se debe ingresar el correo si desea recuperar la cuenta"
+      
+    }else{
+      this.mostrarMess=false;
+      let email : string = this.loginDTO.email;
+
+      this.clinicaService.enviarLinkRecuperacion(email).subscribe({
+        next: data => {
+          this.alerta = { mensaje: data.respuesta, tipo: "success" };
+        },
+        error: error => {
+          this.alerta = { mensaje: error.error.respuesta, tipo: "danger" };
+        }
+      });
+    }   
+  }
 }
